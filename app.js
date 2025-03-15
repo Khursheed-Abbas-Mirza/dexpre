dd88474bb10b8809d59239fd34ea4915f68d337d
Khursheed Abbas <mirzakhursheedabbas14@gmail.com>
2025-03-15 17:54:44 +0530

    Done By ME

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const serverless = require('serverless-http');
-
+const path=require("path")
 const app = express();
 const router = express.Router();
-
+const fs = require('fs');
 // Middleware to parse JSON
 app.use(express.json());
 app.use(express.static('public'));
+const viewsPath = path.join(__dirname, 'views');
+console.log('Views directory:', viewsPath);
+if (!fs.existsSync(viewsPath)) {
+  console.error('Views directory not found:', viewsPath);
+}
 app.set('view engine', 'ejs');
+app.set('views', viewsPath);
 
 // Sample API routes
 router.get('/hello', (req, res) => {
